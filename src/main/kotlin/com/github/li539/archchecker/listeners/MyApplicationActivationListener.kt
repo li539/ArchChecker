1767657faad250fe77caa0976cf296310d144236
GitHub Action <action@github.com>
2024-05-10 02:38:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/li539/archchecker/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/li539/archchecker/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.li539.archchecker.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
